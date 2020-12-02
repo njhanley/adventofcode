f5bb0f499c1cf080b9b7d6af3b3a9097538b2c98
njhanley <njhanley@users.noreply.github.com>
2020-12-02 18:09:03 -0500

    format 2020/01 with prettier

--- 2020/01/part2.js
+++ 2020/01/part2.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("input.txt", "utf-8").split("\n").map(line => parseInt(line, 10));
+const input = fs
+    .readFileSync("input.txt", "utf-8")
+    .split("\n")
+    .map(line => parseInt(line, 10));
 
 for (let i = 0; i < input.length; i++)
     for (let j = i + 1; j < input.length; j++)
