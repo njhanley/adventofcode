086caa683fd3885215400f3c99ec4be67a94c321
njhanley <njhanley@users.noreply.github.com>
2020-12-15 22:03:29 -0500

    2020/15

---
+++ 2020/15/part2.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const fs = require("fs");
+
+Object.prototype.pipe = function (callback) {
+    return callback(this);
+};
+
+fs.readFileSync("input.txt", "utf-8")
+    .split(",")
+    .map(x => +x)
+    .pipe(spoken => {
+        const memory = new Map(spoken.map((n, i) => [n, [i]]));
+        for (let i = spoken.length; i < 30000000; i++) {
+            const [$1, $2] = memory.get(spoken[i - 1]);
+            const n = $2 === undefined ? 0 : $1 - $2;
+            spoken.push(n);
+            memory.set(n, [i, memory.get(n)?.shift()]);
+        }
+        return spoken.pop();
+    })
+    .pipe(console.log);
