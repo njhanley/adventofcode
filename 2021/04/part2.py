30f3d46b7681a8549f6ab1ecb14e8e02c6600780
Nick Hanley <nicholasjhanley@gmail.com>
2021-12-04 01:02:52 -0500

    2021/04

---
+++ 2021/04/part2.py
@@ -0,0 +1,47 @@
+import re
+from itertools import chain
+
+
+def read(filename):
+    with open(filename) as file:
+        return file.read()
+
+
+def match(string, pattern, function):
+    return [function(*match.groups()) for match in re.finditer(pattern, string)]
+
+
+def parse(filename, pattern, function):
+    return match(read(filename), pattern, function)
+
+
+data = read("input.txt").split("\n\n")
+drawings = match(data[0], r"(\d+)", int)
+boards = [
+    [match(line, r"(\d+)", int) for line in entry.splitlines()] for entry in data[1:]
+]
+
+
+def is_winner(board):
+    for row in board:
+        if all(n is None for n in row):
+            return True
+    for column in zip(*board):
+        if all(n is None for n in column):
+            return True
+    return False
+
+
+for n in drawings:
+    winners = []
+    for board in boards:
+        for row in board:
+            for i, m in enumerate(row):
+                if n == m:
+                    row[i] = None
+        if is_winner(board):
+            winners.append(board)
+    boards = [board for board in boards if board not in winners]
+    if len(boards) == 0:
+        print(n * sum(filter(lambda n: n is not None, chain(*winners[-1]))))
+        exit()
