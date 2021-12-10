048de06a997451ace78139f2ec1095aa4511e8eb
Nick Hanley <nicholasjhanley@gmail.com>
2021-12-09 22:59:47 -0500

    2021/08 v2

--- 2021/08/part2.py
+++ 2021/08/part2.py
@@ -15,60 +15,57 @@ def parse(filename, pattern, function):
     return match(read(filename), pattern, function)
 
 
-class Set(frozenset):
+class Pattern(frozenset):
     def __repr__(self):
         return "".join(sorted(self))
 
 
-int_to_wiring = {
-    0: Set("abcefg"),
-    1: Set("cf"),
-    2: Set("acdeg"),
-    3: Set("acdfg"),
-    4: Set("bcdf"),
-    5: Set("abdfg"),
-    6: Set("abdefg"),
-    7: Set("acf"),
-    8: Set("abcdefg"),
-    9: Set("abcdfg"),
+table = {
+    Pattern("abcefg"): 0,
+    Pattern("cf"): 1,
+    Pattern("acdeg"): 2,
+    Pattern("acdfg"): 3,
+    Pattern("bcdf"): 4,
+    Pattern("abdfg"): 5,
+    Pattern("abdefg"): 6,
+    Pattern("acf"): 7,
+    Pattern("abcdefg"): 8,
+    Pattern("abcdfg"): 9,
 }
-wiring_to_int = {v: k for k, v in int_to_wiring.items()}
 
 data = parse(
     "input.txt",
     r"([a-z ]+) \| ([a-z ]+)",
     lambda a, b: (
-        [Set(x) for x in a.split()],
-        [Set(x) for x in b.split()],
+        [Pattern(x) for x in a.split()],
+        [Pattern(x) for x in b.split()],
     ),
 )
 
-answer = 0
+values = []
 for patterns, digits in data:
-    unknown = {
-        pattern: [
-            wiring for wiring in int_to_wiring.values() if len(wiring) == len(pattern)
-        ]
-        for pattern in patterns
-    }
-
     known = {}
+    unknown = {pattern: frozenset(table) for pattern in patterns}
     while unknown:
-        for wiring, candidates in unknown.copy().items():
-            for w1, w2 in known.items():
-                candidates = [
+        for pattern, candidates in unknown.copy().items():
+            for candidate in candidates:
+                candidates = frozenset(
                     candidate
                     for candidate in candidates
-                    if wiring.issubset(w1) == candidate.issubset(w2)
-                    and wiring.issuperset(w1) == candidate.issuperset(w2)
-                ]
-            if len(candidates) == 1:
-                del unknown[wiring]
-                known[wiring] = candidates[0]
-            else:
-                unknown[wiring] = candidates
+                    if len(pattern) == len(candidate)
+                    and all(
+                        pattern.issubset(a) == candidate.issubset(b)
+                        and pattern.issuperset(a) == candidate.issuperset(b)
+                        for a, b in known.items()
+                    )
+                )
+            try:
+                [known[pattern]] = candidates
+                del unknown[pattern]
+            except ValueError:
+                unknown[pattern] = candidates
 
-    mapping = {w1: wiring_to_int[w2] for w1, w2 in known.items()}
-    answer += reduce(lambda a, b: a * 10 + b, (mapping[digit] for digit in digits))
+    mapping = {a: table[b] for a, b in known.items()}
+    values.append(reduce(lambda a, b: a * 10 + b, (mapping[digit] for digit in digits)))
 
-print(answer)
+print(sum(values))
