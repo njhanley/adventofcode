a1c048574014a6ac081c9e14e7c493b98beef602
Nick Hanley <nicholasjhanley@gmail.com>
2021-12-11 00:48:54 -0500

    2021/11

---
+++ 2021/11/part1.py
@@ -0,0 +1,64 @@
+import re
+
+
+def read(filename):
+    with open(filename) as file:
+        return file.read()
+
+
+def match(string, pattern, function):
+    return [function(*match.groups()) for match in re.finditer(pattern, string)]
+
+
+def parse(filename, pattern, function):
+    return match(read(filename), pattern, function)
+
+
+def adjacent(x, y):
+    return [
+        (x + dx, y + dy)
+        for dx, dy in [
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 1),
+            (-1, 0),
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+        ]
+    ]
+
+
+data = parse("input.txt", r"(\d+)", list)
+
+grid = {(x, y): int(n) for y, row in enumerate(data) for x, n in enumerate(row)}
+
+flashes = 0
+for _ in range(100):
+    for point in grid:
+        grid[point] += 1
+
+    flashed = set()
+    while True:
+        flashing = [
+            point
+            for point, energy in grid.items()
+            if point not in flashed and energy > 9
+        ]
+        if not flashing:
+            break
+        for point in flashing:
+            flashed.add(point)
+            for point in adjacent(*point):
+                try:
+                    grid[point] += 1
+                except KeyError:
+                    pass
+
+    for point in flashed:
+        grid[point] = 0
+
+    flashes += len(flashed)
+
+print(flashes)
