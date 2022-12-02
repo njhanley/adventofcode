93f1ffd0f5b14fa9d16e922a01d46a89e7f6bfb8
Nick Hanley <nicholasjhanley@gmail.com>
2022-12-01 20:46:34 -0500

    2022/01 v2

--- 2022/01/part1.rs
+++ 2022/01/part1.rs
@@ -1,9 +1,24 @@
 #!/usr/bin/env rust-script
 
-let answer = std::fs::read_to_string("input.txt")?
+impl<T> Pipe for T {}
+trait Pipe: Sized {
+	fn pipe<B, F>(self, f: F) -> B
+	where
+		F: FnOnce(Self) -> B,
+	{
+		f(self)
+	}
+}
+
+std::fs::read_to_string("input.txt")
+	.unwrap()
 	.split("\n\n")
-	.map(|record| record.lines()
-		.map(|line| line.parse::<u32>().unwrap())
-		.sum::<u32>())
-	.max().unwrap();
-println!("{}", answer);
+	.map(|record| {
+		record
+			.lines()
+			.map(|line| line.parse::<u32>().unwrap())
+			.sum::<u32>()
+	})
+	.max()
+	.unwrap()
+	.pipe(|answer| println!("{answer}"));
--- 2022/01/part2.rs
+++ 2022/01/part2.rs
@@ -1,14 +1,31 @@
 #!/usr/bin/env rust-script
 
-let mut counts = std::fs::read_to_string("input.txt")?
+impl<T> Pipe for T {}
+trait Pipe: Sized {
+	fn pipe<B, F>(self, f: F) -> B
+	where
+		F: FnOnce(Self) -> B,
+	{
+		f(self)
+	}
+}
+
+std::fs::read_to_string("input.txt")
+	.unwrap()
 	.split("\n\n")
-	.map(|record| record.lines()
-		.map(|line| line.parse::<u32>().unwrap())
-		.sum::<u32>())
-	.collect::<Vec<_>>();
-counts.sort();
-let answer = counts.iter()
+	.map(|record| {
+		record
+			.lines()
+			.map(|line| line.parse::<u32>().unwrap())
+			.sum::<u32>()
+	})
+	.collect::<Vec<_>>()
+	.pipe(|mut calories| {
+		calories.sort();
+		calories
+	})
+	.iter()
 	.rev()
 	.take(3)
-	.sum::<u32>();
-println!("{}", answer);
+	.sum::<u32>()
+	.pipe(|answer| println!("{answer}"));
